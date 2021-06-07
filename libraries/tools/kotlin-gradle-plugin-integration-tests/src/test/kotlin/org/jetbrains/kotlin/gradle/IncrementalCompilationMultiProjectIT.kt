ec872fd049eea42c5ac8852bb132179abf5dc6fb
nataliya.valtman <nataliya.valtman@jetbrains.com>
2021-06-07 16:55:16 +0300

    fix up tests

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/IncrementalCompilationMultiProjectIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/IncrementalCompilationMultiProjectIT.kt
@@ -265,7 +265,7 @@ open class A {
         }
 
         //don't need to recompile app classes because lib's proto stays the same
-        project.build("build", "") {
+        project.build("build") {
             assertSuccessful()
             val affectedSources = project.projectDir.allKotlinFiles()
             val relativePaths = project.relativize(affectedSources)
