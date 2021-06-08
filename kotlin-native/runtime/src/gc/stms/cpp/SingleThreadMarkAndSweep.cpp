99619bdcc08ff0d0fda04f9a0d83c01f0f394358
Ilya Matveev <ilya.matveev@jetbrains.com>
2021-06-08 20:42:22 +0700

    Revert "[K/N][Runtime] Lock thread registry in STMS GC"
    This reverts commit 47a8083454512bf49b0bd268fc0a1bffa4905994.

--- kotlin-native/runtime/src/gc/stms/cpp/SingleThreadMarkAndSweep.cpp
+++ kotlin-native/runtime/src/gc/stms/cpp/SingleThreadMarkAndSweep.cpp
@@ -88,31 +88,28 @@ void gc::SingleThreadMarkAndSweep::ThreadData::OnOOM(size_t size) noexcept {
 
 void gc::SingleThreadMarkAndSweep::PerformFullGC() noexcept {
     RuntimeAssert(running_ == false, "Cannot have been called during another collection");
-    mm::ObjectFactory<GC>::FinalizerQueue finalizerQueue;
-    {
-        auto& threadRegistry = mm::GlobalData::Instance().threadRegistry();
-        AutoReset scopedAssign(&running_, true);
-        std::unique_lock threadRegistryLock = threadRegistry.Lock();
+    running_ = true;
 
-        KStdVector<ObjHeader*> graySet;
-        for (auto& thread : threadRegistry.Iter()) {
-            thread.Publish();
-            for (auto* object : mm::ThreadRootSet(thread)) {
-                if (!isNullOrMarker(object)) {
-                    graySet.push_back(object);
-                }
-            }
-        }
-        mm::StableRefRegistry::Instance().ProcessDeletions();
-        for (auto* object : mm::GlobalRootSet()) {
+    KStdVector<ObjHeader*> graySet;
+    for (auto& thread : mm::GlobalData::Instance().threadRegistry().Iter()) {
+        thread.Publish();
+        for (auto* object : mm::ThreadRootSet(thread)) {
             if (!isNullOrMarker(object)) {
                 graySet.push_back(object);
             }
         }
-
-        gc::Mark<MarkTraits>(std::move(graySet));
-        finalizerQueue = gc::Sweep<SweepTraits>(mm::GlobalData::Instance().objectFactory());
     }
+    mm::StableRefRegistry::Instance().ProcessDeletions();
+    for (auto* object : mm::GlobalRootSet()) {
+        if (!isNullOrMarker(object)) {
+            graySet.push_back(object);
+        }
+    }
+
+    gc::Mark<MarkTraits>(std::move(graySet));
+    auto finalizerQueue = gc::Sweep<SweepTraits>(mm::GlobalData::Instance().objectFactory());
+
+    running_ = false;
 
     // TODO: These will actually need to be run on a separate thread.
     // TODO: This probably should check for the existence of runtime itself, but unit tests initialize only memory.
