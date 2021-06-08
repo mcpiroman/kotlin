3449d14dd58d8a1fbf75d948ffff980d1519eab0
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2021-06-08 19:25:27 +0200

    Clean-up coroutines usage in scripting libs and plugin
    #KT-30778 fixed (again)

--- libraries/scripting/common/build.gradle.kts
+++ libraries/scripting/common/build.gradle.kts
@@ -7,7 +7,6 @@ jvmTarget = "1.6"
 
 dependencies {
     compile(kotlinStdlib())
-    compile(commonDep("org.jetbrains.kotlinx", "kotlinx-coroutines-core"))
     compileOnly(project(":kotlin-reflect-api"))
     testCompile(commonDep("junit"))
 }
